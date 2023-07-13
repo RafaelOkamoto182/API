a434e05be16cb5735dad68729e709149bbeaf301
RafaelOkamoto182 <rafa.mg1@hotmail.com>
2023-07-13 11:21:24 -0300

    aprendendo a usar o params

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ const express = require('express')
 
 const app = express()
 
-const PORT = 3333
+app.get("/messages/:id/:user", (req, res) => {
+    const { id, user } = req.params
 
+    res.send(`
+        Mensagem ${id} para o usuario ${user}
+    `)
+
+})
+
+const PORT = 3333
 app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) })
