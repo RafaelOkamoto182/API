62f6c33738d51f035abceb5971dade0eff8cc387
RafaelOkamoto182 <rafa.mg1@hotmail.com>
2023-07-14 11:14:44 -0300

    aprendendo post e instalando nodemon

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 
 const app = express()
+//Define em qual formato as requisições vao vir. Sem isso dá erro na hora de fazer os post etc, só funcionaria os GET
+app.use(express.json())
 
 //route params: os valores SAO OBRIGATORIOS
 app.get("/messages/:id/:user", (req, res) => {
@@ -9,7 +11,6 @@ app.get("/messages/:id/:user", (req, res) => {
     res.send(`
         Mensagem ${id} para o usuario ${user}
     `)
-
 })
 
 //query params: os valores NAO SAO OBRIGATORIOS. Se nao passar nada os valores vao como undefined
@@ -17,6 +18,13 @@ app.get("/users", (req, res) => {
     const { page, limit } = req.query
 
     res.send(`Pagina: ${page}. Limite de usuarios: ${limit}`)
+})
+
+app.post("/users", (req, res) => {
+    const { name, email, password } = req.body
+
+    res.json({ name, email, password })
+
 })
 
 const PORT = 3333
