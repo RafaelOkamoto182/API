de754c52af1a63d0b90ffab6ee76049390219ced
RafaelOkamoto182 <rafa.mg1@hotmail.com>
2023-07-18 16:07:45 -0300

    Conexao com db, instalacao sqlite e sqlite3

--- src/server.js
+++ src/server.js
@@ -2,12 +2,16 @@ const AppError = require("./utils/AppError")
 
 const express = require('express')
 
+const database = require('./database/sqlite')
+
 //Quando nao especifica nenhum arquivo dentro da pasta, ele ja procura algum com nome de "index"
 const routesRouter = require("./routes")
 
 const app = express()
 //Define em qual formato as requisições vao vir. Sem isso dá erro na hora de fazer os post etc, só funcionaria os GET
 app.use(express.json())
+
+database()
 
 app.use(routesRouter)
 
