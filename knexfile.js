4fe84142a279bf5d3742f41d399610a1154d78e5
RafaelOkamoto182 <rafa.mg1@hotmail.com>
2023-08-03 15:45:08 -0300

    criacao das migrations para notas e tags. Habilitacao da deleção em cascata para SQLite

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, 'src', 'database', 'database.db')
     },
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb) //funcao apos a conexao ser criada. Habilita a funcao de deletar em cascata no SQLite. conn= connection; cb = callback
+    },
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
     },
