c223289d8571d29d580290304780fd95c79fdd47
Rafael okamoto <rafa.mg1@hotmail.com>
2023-08-29 20:28:51 -0300

    rota para fazer upload da imagem

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 const path = require('path')
 
 const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp')
-const UPLOADS_FOLDER = path.resolve(__dirname, 'uploads')
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, 'uploads')
 
 const MULTER = {
     destination: multer.diskStorage({
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,22 @@
 const { Router } = require("express")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
+
+const certifyAuthenticated = require("../middlewares/certifyAuthenticated")
+const UserController = require("../controllers/UserController")
 
 const userRouter = Router()
 
-const UserController = require("../controllers/UserController")
-const certifyAuthenticated = require("../middlewares/certifyAuthenticated")
 const userController = new UserController()
 
-//query params: os valores NAO SAO OBRIGATORIOS. Se nao passar nada os valores vao como undefined
-userRouter.get("/", userController.getUsers)
+const upload = multer(uploadConfig.MULTER)
+
 
 userRouter.post("/", userController.create)
 userRouter.put("/", certifyAuthenticated, userController.update)
+userRouter.patch("/avatar", certifyAuthenticated, upload.single("avatar"), (req, res) => {
+    console.log(req.file.filename)
+    res.json()
+})
 
 module.exports = userRouter
